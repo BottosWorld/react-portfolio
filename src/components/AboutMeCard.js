01f02ff2e177d0650cf4f87c61355daeb1c09944
bryan <bryanb@bryanbs-mbp.mynetworksettings.com>
2022-04-17 18:38:11 -0400

    add typewriter package, component, and to about me page

--- src/components/AboutMeCard.js
+++ src/components/AboutMeCard.js
@@ -2,17 +2,23 @@ import React from 'react'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import AboutMeVideo from './AboutMeVideo'
+import AboutMeTypewriter from './AboutMeTypewriter'
+
 
 const AboutMeCard = (props) => {
     return(
         <div>
             <Card bg='dark' border="light">
             <Card.Body>
-                <Card.Header as="h5">A Brief Video About Myself</Card.Header>
-                <Card.Title>*Coming Soon*</Card.Title>
+                <Card.Header as="h5">
+                    A Brief Video About Myself
+                </Card.Header>
+                <Card.Title>
+                    *Coming Soon*
+                </Card.Title>
                 <br></br>
                 <Card.Subtitle>
-                    <AboutMeVideo/>
+                    <AboutMeTypewriter/>
                 </Card.Subtitle>
                 <br></br>
                 <p>---<Button variant="outline-primary" href="https://linkedin.com/in/bryanleebottorff">LinkedIn</Button>---</p>
